78e908b848aea87c4162bdf8867eb28bc98cb610
Luis Costa <luisg.costa@hotmail.com>
2022-03-17 21:43:02 +0000

    Add GameBoard folder and Position class

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using GameBoard;
 
 namespace ChessGame
 {
@@ -6,7 +7,9 @@ namespace ChessGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Position p = new Position(3, 4);
+
+            Console.WriteLine("Position: " + p);
         }
     }
 }
