42dfb9372e68ebc22545a814622fc2e3b4bd7895
Luis Costa <luisg.costa@hotmail.com>
2022-03-18 13:15:48 +0000

    Change BoardPrinter to print white and black pieces in different colors

--- ChessGame/BoardPrinter.cs
+++ ChessGame/BoardPrinter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using GameBoard;
+using GameBoard.Enum;
 
 namespace ChessGame
 {
@@ -11,20 +12,36 @@ namespace ChessGame
         {
             for (int i = 0; i < board.Rank; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < board.Column; j++)
-                {   
+                {
                     if(board.piece(i,j) == null)
                     {
                         Console.Write("- ");
                     }
                     else {
-                        Console.Write(board.piece(i, j) + " ");
+                        PrintPiece(board.piece(i,j));
                     }
                     
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
 
+        }
+
+        public static void PrintPiece(Piece piece)
+        {
+            if(piece.Color == Color.White) {
+                Console.Write(piece + " ");
+            }
+            else
+            {
+                ConsoleColor temp = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(piece + " ");
+                Console.ForegroundColor = temp;
+            }
         }
     }
 }
